2da3203d6f15d6435892fcae55a04c0248a57cb8
pandemuliada <pandemuliada@gmail.com>
2020-05-11 13:13:24 +0800

    :recycle: create plant section responsive on homepage

--- web/components/pages/home/PlantSection.tsx
+++ web/components/pages/home/PlantSection.tsx
@@ -7,11 +7,50 @@ import { useEffect } from 'react'
 import { url } from '../../../utils/api'
 import { PrimaryButton } from '../../primitives/buttons'
 import NextLink from '../../NextLink'
+import useWindowWidth from '../../../hooks/use-window-width'
 
 const Card = dynamic(() => import('../../Card'))
 const Carousel = dynamic(() => import('@brainhubeu/react-carousel'), { ssr: false })
 
+const PlantCarousel: React.FC<any> = ({ data }) => {
+  return (
+    <Carousel
+      keepDirectionWhenDragging
+      centered
+      infinite
+      clickToChange
+      slidesPerPage={2}
+      itemWidth={270}
+      breakpoints={{
+        480: {
+          infinite: false,
+        },
+      }}
+    >
+      {data.map((plant: any, index: number) => (
+        <Card
+          width="100%"
+          height={370}
+          key={`${index}_${plant.name}`}
+          title={plant.name}
+          subtitle={plant.category.name}
+          mr="20px"
+          image={{ src: url(`/${plant.picture}`), alt: plant.title }}
+        >
+          <NextLink href={`catalogs/${plant.id}`} display="block" mt="20px" _hover={{ textDecoration: 'none' }}>
+            <PrimaryButton as="span" display="block" width="75%" mx="auto">
+              Detail
+            </PrimaryButton>
+          </NextLink>
+        </Card>
+      ))}
+    </Carousel>
+  )
+}
+
 const PlantSection: React.FC = () => {
+  const [windowWidth] = useWindowWidth()
+
   const Dispatch = useDispatch()
   const plants = useSelector((state: iRootState) => state.Plants)
 
@@ -41,25 +80,13 @@ const PlantSection: React.FC = () => {
           Tanaman yang banyak dicari oleh pelanggan yang bisa jadi referensi anda
         </Text>
       </Container>
-      <Carousel keepDirectionWhenDragging centered clickToChange slidesPerPage={2} itemWidth={270}>
-        {plants.data.map((plant: any, index: number) => (
-          <Card
-            width="100%"
-            height={370}
-            key={`${index}_${plant.name}`}
-            title={plant.name}
-            subtitle={plant.category.name}
-            mr="20px"
-            image={{ src: url(`/${plant.picture}`), alt: plant.title }}
-          >
-            <NextLink href={`catalogs/${plant.id}`} display="block" mt="20px" _hover={{ textDecoration: 'none' }}>
-              <PrimaryButton as="span" display="block" width="75%" mx="auto">
-                Detail
-              </PrimaryButton>
-            </NextLink>
-          </Card>
-        ))}
-      </Carousel>
+      {windowWidth > 720 ? (
+        <Container mx="auto">
+          <PlantCarousel data={plants.data} />
+        </Container>
+      ) : (
+        <PlantCarousel data={plants.data} />
+      )}
     </Box>
   )
 }
